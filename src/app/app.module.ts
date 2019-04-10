d421ecc94f6ca95a63cc6c6e380139ad58bb2f51
Norberto Caceres <norbe1994@yahoo.com>
2019-04-10 10:10:49 -0500

    crear servicio de peliculas y componentes: home, navbar y pelicula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { AppRoutingModule } from './app-routing.module'
+// componentes
+import { AppComponent } from './app.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { HomeComponent } from './components/home/home.component'
+import { PeliculaComponent } from './components/pelicula/pelicula.component'
+// servicios
+import { PeliculasService } from './providers/peliculas.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, PeliculaComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [PeliculasService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
