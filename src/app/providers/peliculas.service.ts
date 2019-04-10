c287dcbc0164363685e7074dfcc70705ccb76063
Norberto Caceres <norbe1994@yahoo.com>
2019-04-10 10:35:04 -0500

    crear metodo getCartelera

--- src/app/providers/peliculas.service.ts
+++ src/app/providers/peliculas.service.ts
@@ -9,6 +9,22 @@ export class PeliculasService {
 
   constructor(private http: HttpClient) {}
 
+  getCartelera() {
+    let desde = new Date()
+    let hasta = new Date()
+    // mover una semana al futuro
+    hasta.setDate(hasta.getDate() + 7)
+    let desdeString = `${desde.getFullYear()}-${desde.getMonth()}-${desde.getDate()}`
+    let hastaString = `${hasta.getFullYear()}-${hasta.getMonth()}-${hasta.getDate()}`
+    let url = `${
+      this.urlMoviedb
+    }/discover/movie?primary_release_date.gte=${desdeString}&primary_release_date.lte=${hastaString}&api_key=${
+      this.apikey
+    }&language=es&callback=JSONP_CALLBACK`
+
+    return this.http.jsonp(url, '').pipe(map(res => res))
+  }
+
   getPopulares() {
     let url = `${this.urlMoviedb}/discover/movie?sort_by=popularity.desc&api_key=${
       this.apikey
